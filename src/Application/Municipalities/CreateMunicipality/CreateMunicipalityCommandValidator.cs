63be417bd8d87333b602dce56fd90a372164ef98
ThomasStarkInc <thst@danskespil.dk>
2025-02-09 20:05:33 +0100

    Adding a validator for CreateMunicipalityCommand

---
+++ src/Application/Municipalities/CreateMunicipality/CreateMunicipalityCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Municipalities.CreateMunicipality;
+
+using FluentValidation;
+
+namespace Application.Todos.Create;
+
+public class CreateMunicipalityCommandValidator : AbstractValidator<CreateMunicipalityCommand>
+{
+    public CreateMunicipalityCommandValidator()
+    {
+        RuleFor(c => c.Name).NotEmpty().MaximumLength(2).MaximumLength(100);
+    }
+}
